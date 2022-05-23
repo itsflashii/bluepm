31d757c4590cf3f3409ac3a4f03260e3fe573d3b
Vítor Ribeiro <85264247+itsSoweto@users.noreply.github.com>
2022-05-23 06:38:01 -0300

    Update require.lua

--- logic/require.lua
+++ logic/require.lua
@@ -1,11 +1,3 @@
---[[
-
-* @overview Logic for importing new modules within the blue
-* @author Vítor 'Soweto' Ribeiro
-* @copyright (c) Rocket Community
-* @license See LICENSE file on top-level directory
-
--]]
 function require(attrName)
   for index, module in ipairs(modules) do
     if (modules[index].name == attrName) then
