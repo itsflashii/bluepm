db0a278a8334d3e2c712f1c9b8ef1490ac13fc7d
Vítor Ribeiro <85264247+itsSoweto@users.noreply.github.com>
2022-05-23 06:38:25 -0300

    Update test-myid.lua

--- tests/test-myid.lua
+++ tests/test-myid.lua
@@ -1,11 +1,3 @@
---[[
-
-* @overview Test file getting information from a certain foundation on the Brazilian stock exchange
-* @author Vítor 'Soweto' Ribeiro
-* @copyright (c) Rocket Community
-* @license See LICENSE file on top-level directory
-
--]]
 local id = require("myid");
 
 addCommandHandler("test.myid", function()
