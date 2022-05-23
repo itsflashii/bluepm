5f9946db2755ba3dbfa5db2072e951c789947aec
Vítor Ribeiro <85264247+itsSoweto@users.noreply.github.com>
2022-05-23 06:37:49 -0300

    Update exports.lua

--- logic/exports.lua
+++ logic/exports.lua
@@ -1,11 +1,3 @@
---[[
-
-* @overview Export logic for packages created with blue
-* @author Vítor 'Soweto' Ribeiro
-* @copyright (c) Rocket Community
-* @license See LICENSE file on top-level directory
-
--]]
 module = {
   exports = function (attrName, attrInstance)
     table.insert(modules, {name=attrName, instance=attrInstance})
